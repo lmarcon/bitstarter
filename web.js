0a4a71afcda55580fd03feb5db96092915bdd100
Lorenzo Marcon <lorenzo.marcon@gmail.com>
2013-07-10 07:40:18 +0000

    Try to fix unknown encoding error on heroku

--- web.js
+++ web.js
@@ -5,10 +5,8 @@ var app = express.createServer(express.logger());
 var fs = require('fs');
 
 app.get('/', function(request, response) {
-  var buf = fs.readFileSync('index.html', {encoding: 'utf-8'}, function (err, data) {
-    if (err) throw err;
-  });
-  response.send(buf);
+  var buf = fs.readFileSync('index.html');
+  response.send(buf.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
