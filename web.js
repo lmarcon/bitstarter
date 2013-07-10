3bbf70d604540b48c298df895f638cf56916a86c
Lorenzo Marcon <lorenzo.marcon@gmail.com>
2013-07-10 07:15:00 +0000

    using buffer.toString()

--- web.js
+++ web.js
@@ -5,9 +5,9 @@ var app = express.createServer(express.logger());
 var fs = require('fs');
 
 app.get('/', function(request, response) {
-  fs.readFileSync('index.html', {encoding: 'utf8'}, function (err, data) {
+  fs.readFileSync('index.html', function (err, data) {
     if (err) throw err;
-    response.send(data);
+    response.send(data.toString());
   });
 });
 
